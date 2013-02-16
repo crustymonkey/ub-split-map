ef9f6c2aea81ff55e9411139788ccef836a6aae1
Jay Deiman <jay@splitstreams.com>
2013-02-15 16:56:19 -0800

    Started actually coding the logic

--- ubsplitmap.py
+++ ubsplitmap.py
@@ -5,10 +5,29 @@ that will map external IPs to RFC-1918 IPs.  This would be a replacement
 for split horizon DNS that would work in a dynamic fashion.
 """
 
-from configparser import SafeConfigParser
+from ConfigParser import SafeConfigParser
+import os
+
+CONFIG_SEARCH = [
+    '/etc/ub-split-map.ini' ,
+    '/etc/unbound/ub-split-map.ini' ,
+    '/usr/local/etc/ub-split-map.ini' ,
+    '/usr/local/etc/unbound/ub-split-map.ini' ,
+]
+
+if 'HOME' in os.environ:
+    CONFIG_SEARCH.extend([
+        os.path.join(os.environ['HOME'] , 'ub-split-map.ini') ,
+        os.path.join(os.environ['HOME'] , '/unbound/ub-split-map.ini') ,
+    ])
+
+class Globals(object):
+    conf = None
 
 def getConf():
-    pass
+    conf = SafeConfigParser()
+    conf.read(CONFIG_SEARCH)
+    return conf
 
 #
 # Unbound hooks
@@ -17,6 +36,7 @@ def init(mid , cfg):
     """
     Called upon initialization
     """
+    Globals.conf = getConf()
     return True
 
 def deinit(mid):
@@ -29,4 +49,15 @@ def inform_super(mid , qstate , superqstate , qdata):
     return True
 
 def operate(mid , event , qstate , qdata):
+    conf = Globals.conf
+    if event in (MODULE_EVENT_NEW , MODULE_EVENT_PASS):
+        qstate.ext_state[mid] = MODULE_WAIT_MODULE
+        return True
+
+    if event == MODULE_EVENT_MODDONE:
+        if not qstate.return_msg or not qstate.return_msg.rep:
+            qstate.ext_state[mid] = MODULE_FINISHED
+            return True
+        qn = qstate.qinfo.qname_str.rstrip('.')
+
     return True
